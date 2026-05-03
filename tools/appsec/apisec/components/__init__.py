fa6a3758be6d58daba479daf79969763680778d6
Paulo Dreher <48896261+paulodreher@users.noreply.github.com>
2026-05-03 16:53:17 -0300

    Delete tools/appsec/apisec directory

--- tools/appsec/apisec/components/__init__.py
+++
@@ -1,6 +0,0 @@
-# components package
-from components.vuln_detail import render_vuln_detail
-from components.qa_panel import render_qa_panel
-from components.summary import render_summary
-
-__all__ = ["render_vuln_detail", "render_qa_panel", "render_summary"]
--- tools/appsec/apisec/data/__init__.py
+++
@@ -1,5 +0,0 @@
-# data package
-from data.vulnerabilities import VULNERABILITIES
-from data.knowledge_base import KnowledgeBase
-
-__all__ = ["VULNERABILITIES", "KnowledgeBase"]
